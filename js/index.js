cd21b9f7b1990081660fa97249811c6e2a178d67
Jerry Simmons GISP <fcemc@users.noreply.github.com>
2016-12-08 16:03:46 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -42,23 +42,23 @@ function onResume() {
 }
 
 
-function checkForSync() {
-    if (localStorage.getItem("allInspections") != "" && localStorage.getItem("allInspections") != null) {       
-        var ai = JSON.parse(localStorage.getItem("allInspections"));
-        $("#syncBtn").css("visibility", "visible");
-        if (ai.length > 0) {
-            if (navigator.onLine) {
+//function checkForSync() {
+//    if (localStorage.getItem("allInspections") != "" && localStorage.getItem("allInspections") != null) {       
+//        var ai = JSON.parse(localStorage.getItem("allInspections"));
+//        $("#syncBtn").css("visibility", "visible");
+//        if (ai.length > 0) {
+//            if (navigator.onLine) {
 
-                $("#syncBtn").html("Sync Inspections (" + ai.length + ")");
-                $("#syncBtn").css("background-color", "orange");
-            }
-            else {
-                $("#syncBtn").html("Inspections that need to be Synced (" + ai.length + ")");
-                $("#syncBtn").css("background-color", "red");
-            }
-        }
-    }
-    else {
-        $("#syncBtn").css("visibility", "hidden");
-    }
-}
+//                $("#syncBtn").html("Sync Inspections (" + ai.length + ")");
+//                $("#syncBtn").css("background-color", "orange");
+//            }
+//            else {
+//                $("#syncBtn").html("Inspections that need to be Synced (" + ai.length + ")");
+//                $("#syncBtn").css("background-color", "red");
+//            }
+//        }
+//    }
+//    else {
+//        $("#syncBtn").css("visibility", "hidden");
+//    }
+//}
